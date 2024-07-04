5e692349d919076bdaa461a76aa9ed15f4c79460
MthoMSY <mthokozisi.myeza@chillisoft.co.za>
2024-07-04 10:32:10 +0200

    remove console log

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { typeOrmConfig } from './config/typeorm.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(typeOrmConfig);
   await app.listen(3000);
 }
 bootstrap();
