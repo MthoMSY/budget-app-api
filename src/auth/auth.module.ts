b5e6286c663bc9a23853b2fdb264cbd433c54723
MthoMSY <mthokozisi.myeza@chillisoft.co.za>
2024-07-04 13:17:11 +0200

    set up passport and jwt modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,20 @@ import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'mySecret33',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
