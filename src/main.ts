9989264ba5f9727f5a661668ccf4222d12ac3875
MthoMSY <mthokozisi.myeza@chillisoft.co.za>
2024-07-24 11:45:06 +0200

    log port which app is running on

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.enableCors();
-  await app.listen(3000);
-
+  const apiPort = configService.get('API_PORT');
+  await app.listen(apiPort || 3000);
+  console.log(`App is running on port: ${apiPort}`);
 }
 bootstrap();
